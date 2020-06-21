66308479d82d9b61774c93883f63a2bfcf5fde31
kakke <pont3699@gmail.com>
2020-06-21 11:31:34 +0900

    git ignore api key

--- src/Organisms/IdeasFactory.js
+++ src/Organisms/IdeasFactory.js
@@ -1,13 +1,62 @@
-import React from 'react';
+import React ,{ useState, useReducer,useEffect  } from 'react';
 import CountAndHistory from '../Molecules/CountAndHistory'
 import SendIdea from '../Molecules/SendIdea'
 import GetIdea from '../Molecules/GetIdea'
+import firebase from "firebase";
+import "firebase/storage";
+import reducer from '../reducers'
 
 const IdeasFactory = () => {
+  const test = () => {
+    return console.log("a")
+  }
+  useEffect(()=>{
+
+    return test
+  }
+  )
+  const [state, dispatch] = useReducer(reducer,[])
+  const [ideas, setIdeas] = useState([])
+  const addFireData = (idea) => {
+    let db = firebase.database();
+    let ref = db.ref('ideas/3');
+    ref.set({idea: idea})
+  }
+  const getFireData = () => {
+    let db = firebase.database();
+    let ref = db.ref('ideas');
+    ref.on('value', (snapshot)=> {
+      console.log(snapshot.val())
+      setIdeas(snapshot.val())
+      snapshot.val()
+      setIdeas([...ideas,snapshot.val()])
+    });
+    console.log(ideas)
+  }
+  
+  const getLastID = () => {
+    let db = firebase.database();
+    let ref = db.ref("ideas/");
+    let self = this;
+    let hoge;
+    ref
+      .orderByKey()
+      .limitToLast(1)
+      .on("value", snapshot => {
+        let res = snapshot.val();
+        console.log(res)
+        for (let i in res) {
+          console.log(res[i].id);
+          hoge = res[i].id;
+          // return res[i].id;
+        }
+      });
+    return hoge
+  }
   return(
     <div>
       <CountAndHistory/>
-      <SendIdea/>
+      <SendIdea data={addFireData}/>
       <GetIdea/>
     </div>
   )
